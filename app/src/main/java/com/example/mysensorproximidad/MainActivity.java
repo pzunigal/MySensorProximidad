6e83a3c7bc52a6a7082674e4d88f74b8112f3ea1
pzunigal <zuniga.lagosp@gmail.com>
2023-11-09 15:16:10 -0300

    Se crea el codigo del sensor importando algunas librerias

--- app/src/main/java/com/example/mysensorproximidad/MainActivity.java
+++ app/src/main/java/com/example/mysensorproximidad/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.mysensorproximidad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    LinearLayout ln;
+    SensorManager sm;
+    Sensor sensor;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ln = findViewById(R.id.activity_main);
+        tv = findViewById(R.id.tv);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sm.registerListener(this,sensor , SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        String text = String.valueOf(event.values[0]);
+        //No se porque me da error en el event, compare con otro compañero y a mi no mas me sale ;-;
+        tv.setText(text);
+        float valor = Float.parseFloat(text);
+        if (valor == 0) {
+            ln.setBackgroundColor(Color.GREEN);
+        } else {
+            ln.setBackgroundColor(Color.RED);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
